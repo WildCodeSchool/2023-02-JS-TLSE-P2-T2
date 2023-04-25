7bf30000dd568ac3debf5917110aaa2e5899577f
Jasmine Grozinger <jasmine.grozinger@gmail.com>
2023-04-25 11:59:40 +0200

    S03_U01 installed package npm and add logos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
-import Home from "./pages/Home";
-
 import "./App.css";
+// theme
+import "primereact/resources/themes/lara-dark-indigo/theme.css";
+// core
+import "primereact/resources/primereact.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Main from "./pages/Main";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="main" element={<Main />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
